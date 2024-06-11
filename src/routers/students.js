470161776b98b0eae0c1732c6586282438eb7f72
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-11 18:50:36 +0300

    lesson 1

--- src/routers/students.js
+++ src/routers/students.js
@@ -11,6 +11,8 @@ import {
   patchStudentController,
 } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createStudentSchema, updateStudentSchema } from '../validation/students.js';
 
 
 const router = Router();
@@ -19,13 +21,13 @@ router.get('/students', ctrlWrapper(getStudentsController));
 
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
-router.post('/students', ctrlWrapper(createStudentController));
+router.post('/students', validateBody(createStudentSchema), ctrlWrapper(createStudentController));
 
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
 
 router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
 
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.patch('/students/:studentId', validateBody(updateStudentSchema), ctrlWrapper(patchStudentController));
 
 export default router;
 
