627dec26df0ae1dbc37e9b7cb42662308d863265
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-21 15:11:39 +0300

    reset-password

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -18,3 +18,7 @@ export const requestResetEmailSchema = Joi.object({
   email: Joi.string().email().required(),
 });
 
+export const resetPasswordSchema = Joi.object({
+  password: Joi.string().required(),
+  token: Joi.string().required(),
+});
