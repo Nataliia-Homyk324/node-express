64b00ab22f34ea178ab0689765679fe7f914a325
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-22 17:14:00 +0300

    photos

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -4,7 +4,8 @@ import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
-
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 
 
@@ -108,9 +109,19 @@ export const patchStudentController = async (req, res, next) => {
 
   let photoUrl;
 
-  if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+ if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
+
+
+
+  // if (photo) {
+  //   photoUrl = await saveFileToUploadDir(photo);
+  // }
 
   const result = await updateStudent(studentId, {
     ...req.body,
